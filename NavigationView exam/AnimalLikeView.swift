dd9e9b82d5f118166998374f40390fd5093e21ae
백대홍 <bdh3620@naver.com>
2023-01-19 19:00:38 +0900

    AnimalTabView + AnimalLikeView add

---
+++ NavigationView exam/AnimalLikeView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalLikeView.swift
+//  NavigationView exam
+//
+//  Created by 백대홍 on 2023/01/19.
+//
+
+import SwiftUI
+
+struct AnimalLikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimalLikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalLikeView()
+    }
+}
---
+++ NavigationView exam/AnimalTabView.swift
@@ -0,0 +1,31 @@
+//
+//  AnimalTabView.swift
+//  NavigationView exam
+//
+//  Created by 백대홍 on 2023/01/19.
+//
+
+import SwiftUI
+
+struct AnimalTabView: View {
+    var body: some View {
+        TabView{
+                    AnimalMainView()
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("Main")
+                        }
+                    
+                    AnimalLikeView()
+                        .tabItem {
+                            Image(systemName: "heart.fill")
+                            Text("Like")
+                        }
+                }
+            }
+        }
+struct AnimalTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalTabView()
+    }
+}
